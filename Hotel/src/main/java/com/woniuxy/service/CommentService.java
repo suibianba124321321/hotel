db6ca765414a6848a85514ef861bf507ef0f1776
LX11788528 <1467850193@qq.com>
2019-06-22 15:21:53 +0800

    添加支付依赖包

--- Hotel/src/main/java/com/woniuxy/service/CommentService.java
+++
@@ -1,9 +0,0 @@
-package com.woniuxy.service;
-
-import java.util.List;
-
-import com.woniuxy.pojo.Comment;
-
-public interface CommentService {
-	public List<Comment> allComments();
-}
--- Hotel/src/main/java/com/woniuxy/service/PersonService.java
+++
@@ -1,7 +0,0 @@
-package com.woniuxy.service;
-
-import com.woniuxy.pojo.Person;
-
-public interface PersonService {
-	public void addPerson(Person person,int uid);
-}
